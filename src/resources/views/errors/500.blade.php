d42370f854247ad9fb1850862477530f0e6f4e92
Granko15 <granko15@gmail.com>
2022-12-20 14:51:50 +0100

    header and footer removed

--- src/resources/views/errors/500.blade.php
+++ src/resources/views/errors/500.blade.php
@@ -1,9 +1,21 @@
-@include('common.navigation')
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+        <meta http-equiv="X-UA-Compatible" content="ie=edge">
+        <link rel="icon" type="image/x-icon" href="/images/credit-card-fill.svg">
+        <link href="/css/main.css" rel="stylesheet">
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
+        <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.3.0/font/bootstrap-icons.css">
+        <script src="/js/main.js" rel="stylesheet"></script>
+        <title>ERROR 500</title>
+    </head>
+    <body>
+        <div class="error_div">
+            <span>500 Internal Server Error:</span>
+            <span>Interná chyba servera alebo nesprávna konfigurácia a vašu žiadosť sa nepodarilo dokončiť.</span>
+        </div>
 
-<div class="error_div">
-    <span>500 Internal Server Error:</span>
-    <span>Interná chyba servera alebo nesprávna konfigurácia a vašu žiadosť sa nepodarilo dokončiť.</span>
-
-</div>
-
-@include('common.footer')
+    </body>
+</html>
